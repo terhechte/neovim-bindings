356cbb2966f1aa29c726938e133dfe9ef779be4f
Benedikt Terhechte <terhechte@gmail.com>
2022-03-20 11:20:50 +0100

    Playground

---
+++ playground.lua
@@ -0,0 +1,16 @@
+local function visual_selection_range()
+  local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+  local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  if csrow < cerow or (csrow == cerow and cscol <= cecol) then
+    return csrow - 1, cscol - 1, cerow - 1, cecol
+  else
+    return cerow - 1, cecol - 1, csrow - 1, cscol
+  end
+end
+
+function make_selection()
+  --local a, b, c, d = visual_selection_range()
+  --print(a, b, c, d)
+  vim.cmd('execute "normal! \"+y"')
+end
+
